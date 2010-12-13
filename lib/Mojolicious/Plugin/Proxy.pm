80548c3c64aa8cd4cadbebe76557e951b3a48c61
Marcus Ramberg <marcus@nordaaker.com>
2010-12-13 20:25:42 +0100

    Add docs

--- lib/Mojolicious/Plugin/Proxy.pm
+++ lib/Mojolicious/Plugin/Proxy.pm
@@ -39,4 +39,44 @@ __END__
 
 =head1 NAME
 
-Mojolicious::Plugin::Proxy - Proxy requests to 
+Mojolicious::Plugin::Proxy - Proxy requests to a backend server
+
+=head1 SYNOPSIS
+
+   plugin 'proxy';
+
+   get '/foo' => sub { shift->proxy_to('http://mojolicio.us/') };
+
+=head1 DESCRIPTION
+
+Proxy requests to backend URL using L<Mojo::Client>.
+
+=head1 METHODS
+
+=head2 proxy_to $url, [%options]
+
+Proxies the current request to $url using the L<Mojo::Client> get method.
+supports one parameter:
+
+=over 4
+
+=item with_query_params
+
+If this parameter is set to 1, will get query parameters from the current 
+request and proxy them to the backend.
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008-2010, Marcus Ramberg.
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=cut
+
+
+=head1 SEE ALSO
+
+L<Mojolicious>, L<Mojolicious::Lite>
